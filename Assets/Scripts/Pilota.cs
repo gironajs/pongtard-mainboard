21e29950ea3d5a8119fc35fffc4ee00f2e032561
NidoNyles <nido386@gmail.com>
2017-11-18 16:03:01 +0100

    Ball respawnin on invisible

--- Assets/Scripts/Pilota.cs
+++ Assets/Scripts/Pilota.cs
@@ -5,29 +5,37 @@ using UnityEngine;
 public class Pilota : MonoBehaviour
 {
     private Rigidbody rb;
+    private Vector3 initPos;
     public float speed = 10f;
 
-	// Use this for initialization
-	void Awake ()
+
+    // Use this for initialization
+    void Awake()
     {
         rb = GetComponent<Rigidbody>();
-        rb.AddForce(new Vector3(speed, 0f, 0f), ForceMode.Impulse);
-	}
+        initPos = transform.position;
+        rb.AddForce(new Vector3(speed * 1.1f, speed, 0f), ForceMode.Impulse);
+    }
 
     private void Start()
     {
 
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
         rb.velocity = new Vector3(speed, 0f, 0f);
-        //trans.position += new Vector3(Time.deltaTime * speed, 0f, 0f);
-	}
+        //***NO***trans.position += new Vector3(Time.deltaTime * speed, 0f, 0f);
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
-        speed = speed * -1; 
+        speed = speed * -1;
+    }
+
+    private void OnBecameInvisible()
+    {
+        transform.position = initPos;
     }
 }

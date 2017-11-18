2838956ee456190796f9252c0a573aab3d224a25
NidoNyles <nido386@gmail.com>
2017-11-18 17:30:43 +0100

    pad zones for ball bounce

---
+++ Assets/Scripts/ColliderId.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderId : MonoBehaviour
+{ 
+    public int pos = 0;
+	public int Pos { get { return pos; }}
+}
